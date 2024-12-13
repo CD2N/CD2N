cfc7eb6530a05b0bcfb32e630c410ac60befd37c
ytqaljn <2716693942@qq.com>
2024-12-13 11:53:04 +0800

    feat: submit cache contract

---
+++ cache_contract/ignition/modules/CacheProtocol.js
@@ -0,0 +1,8 @@
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+// npx hardhat ignore
+module.exports = buildModule("CacheProtocol", (m) => {
+  const contract = m.contract("CacheProtocol", ["0x636b7E4E9b7331047b431179809F3546e1f7A841"]);
+
+  return { contract };
+});
