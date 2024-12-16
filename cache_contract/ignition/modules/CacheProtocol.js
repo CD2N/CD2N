ca982f7d7dcea2e5b0dc5119380ecac547f44ce0
ytqaljn <2716693942@qq.com>
2024-12-16 13:54:42 +0800

    feat: add new function

--- cache_contract/ignition/modules/CacheProtocol.js
+++ cache_contract/ignition/modules/CacheProtocol.js
@@ -1,6 +1,6 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
-// npx hardhat ignore
+// npx hardhat ignition deploy ./ignition/modules/CacheProtocol.js --network cessdev
 module.exports = buildModule("CacheProtocol", (m) => {
   const contract = m.contract("CacheProtocol", ["0x636b7E4E9b7331047b431179809F3546e1f7A841"]);
 
